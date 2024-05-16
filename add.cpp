505314cd7972ae96f6bee23165e6906788214b90
soniyadhupdale <159905417+soniyadhupdale@users.noreply.github.com>
2024-05-16 23:43:45 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"enter num1 :\n";
+	cin>>num1;
+	cout<<"enter num2 :"<<endl;
+	cin>>num2;
+	int sum=num1+num2;
+	cout<<"sum="<<sum;
+}
