13febd6cc3627789e2d01153fdad5ebbfdda0972
soniyadhupdale <159905417+soniyadhupdale@users.noreply.github.com>
2024-06-14 21:28:52 +0530

    Add files via upload

---
+++ 3-variable.cpp
@@ -0,0 +1,18 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+	// int a=4;
+	// int b=5;
+	
+	int a=4,b=5;
+	float pi=3.14;
+	char c='s';
+	// donot use more than single character for char
+	
+	cout<<"This is tutorial 4.\nHere, the value of a is : "<<a<<"\nAnd , the value of b is : "<<b;
+	cout<<"\nThe value of pi is : "<<pi;
+	cout<<"\nThe value of c is : "<<c;
+	
+	return 0;
+}
